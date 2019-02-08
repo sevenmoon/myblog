2dbdcc838dd595d5eb6d86a10ce256d6193156ed
lilu <675221350@qq.com>
2019-02-08 13:47:46 +0800

    Add files via upload

---
+++ precache-manifest.7185278d4450c6b6ed2d70627efc5576.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1be8eb0ab124102984e25b2d02d284af",
+    "url": "./static/media/mybook.1be8eb0a.jpg"
+  },
+  {
+    "revision": "3cedb2f65fbbdb43bdd83a692f633153",
+    "url": "./static/media/head.3cedb2f6.png"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "a6fff80a6ae439f37cd6",
+    "url": "./static/js/main.a6fff80a.chunk.js"
+  },
+  {
+    "revision": "0d7d3fa6608ff3ff7b02",
+    "url": "./static/js/1.0d7d3fa6.chunk.js"
+  },
+  {
+    "revision": "a6fff80a6ae439f37cd6",
+    "url": "./static/css/main.4a8bfe46.chunk.css"
+  },
+  {
+    "revision": "0d7d3fa6608ff3ff7b02",
+    "url": "./static/css/1.b3e33e9b.chunk.css"
+  },
+  {
+    "revision": "347c5c38e289db97ec97c74e0d63a0b6",
+    "url": "./index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "./precache-manifest.7185278d4450c6b6ed2d70627efc5576.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("./index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
